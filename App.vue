533a4963dceb7091eb3843d764bbe6a42a897110
A_ShChe <65690436+sweschm19@users.noreply.github.com>
2020-11-23 23:17:56 +0900

    Update App.vue

--- App.vue
+++ App.vue
@@ -1,35 +1,63 @@
 <template>
 <div>
   <GoodHeader></GoodHeader>
-  <p>{{ number}}</p>
-  <GoodNum :number="number" v-on:my-click="number = $event"></GoodNum>
-  <GoodNum :number="number" ></GoodNum>
+  <GoodNum></GoodNum>
+  <button @click="currentComponent= 'Home'">Home</button>
+  <button @click="currentComponent= 'About'">About</button>
+  <keep-alive>
+    <component :is="currentComponent"></component>
+  </keep-alive>
+  <div>
+   <h2>イベントのフォーム</h2>
+   <label for="title">入力内容</label>
+   <input id="title" type="text" 
+   v-model.number="eventData.title"> 
+   <pre>{{eventData.title}}</pre>
+   <label for="maxNumber">最大人数</label>
+   <input id="maxNumber" type="number" 
+      v-model.number="eventData.maxNumber"> //event(object)
+   <p>{{typeof eventData.maxNumber}}</p>
+   <label for="host">Hosting side</label>
+   <input id="host" type="text" 
+   v-model.trim="eventData.host"> 
+   <p>{{eventData.host}}</p>
+   <label for="details">イベントの内容</label>
+   <textarea id="details" cols="30" rows="10"
+   v-model="eventData.details">
+   </textarea>
+   <pre>{{eventData.details}}</pre>
+  </div>
 </div>
   
 </template>
 
 <script>
-import GoodHeader from "./GoodHeader";
+import GoodHeader from "@/components/GoodHeader.vue";
+import About from "@/components/About.vue";
+import Home from "@/components/Home.vue";
 
-export default{
+export default {
   data() {
-    return{
-      number: 10
-    }  
+    return {
+      number: 10,
+      currentComponent: "Home",
+      eventData: {
+        title:"入力内容",
+        maxNumber: 0,
+        host:" ",
+        details:""
+          
+      }
+    };
   },
   components: {
-    GoodHeader
-  } ,
+    GoodHeader,
+    About,
+    Home
+  },
   methods: {
-    increment() {
-      this.$emit("my-click", this.number + 3);
+    incrementNumber(value) {
+      this.number = value
     }
-  } 
-};
-</script>
-
-<style scoped>
-div{
-  border: 1px solid blue;
+  }
 }
-</style>
