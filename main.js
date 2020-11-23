1ccb6565c2901b9d4032e381e82168e64849b64e
A_ShChe <65690436+sweschm19@users.noreply.github.com>
2020-11-23 23:14:48 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
-import GoodNum from "./GoodNum.vue";
+import GoodNum from "@/components/GoodNum.vue";
 
 Vue.config.productionTip = false;
-Vue.component('GoodNum', GoodNum );
+Vue.component('GoodNum', GoodNum )
 
 new Vue({
   render: h => h(App)
 }).$mount('#app');
+
